d6a206963d033ae3622baa8bc810fd70739a89cd
Joezc <lzc814310057@163.com>
2017-05-22 11:45:19 +0800

    add three features, 82.9%

--- xgboost/load_new_feat.py
+++ xgboost/load_new_feat.py
@@ -17,18 +17,22 @@ def load_old_feat():
 
 def load_feat():
     spammer_order = "../feature.json"
-    all_feat_list = ['figure_RRT', 'average_comm', 'figure_at_sigle', 'hashtag', 'reputation',
-                     'late_night_times', 'followee_num', 'follow_ratio', 'post_num', 'figure_at',
-                     'active_day_ratio', 'mention', 'day_interval_variance', 'figure_at_every',
-                     'test_similarity', 'text_similarity', 'url', 'time_interval_mean', 'time_interval_var']
+    #all_feat_list = ['figure_RRT', 'average_comm', 'figure_at_sigle', 'hashtag', 'reputation',
+    #                 'late_night_times', 'followee_num', 'follow_ratio', 'post_num', 'figure_at',
+    #                 'active_day_ratio', 'mention', 'day_interval_variance', 'figure_at_every',
+    #                 'test_similarity', 'text_similarity', 'url', 'time_interval_mean', 'time_interval_var',
+    #                 'followers_num', 'following_num', 'content_length']
 
+    all_feat_list = ['content_length', 'hashtag', 'late_night_times', 'figure_at', 'reputation', 'followee_num', 'followers_num', 'figure_at_every', 'following_num', 'figure_at_sigle', 'active_day_ratio', 'url', 'time_interval_mean', 'test_similarity', 'figure_RRT', 'follow_ratio', 'day_interval_variance', 'text_similarity', 'time_interval_var', 'average_comm', 'post_num', 'mention']
+    
+    
     raw_file = pd.read_json(open(spammer_order), orient='records')
 
     idx_title = ['id'] + all_feat_list + ['is_spammer']
-    raw_file.rename(columns={i: idx_title[i] for i in range(21)}, inplace=True)
+    raw_file.rename(columns={i: idx_title[i] for i in range(len(idx_title))}, inplace=True)
 
     aval_feat_list = ['hashtag', 'reputation', 'post_num', 'mention', 'test_similarity', 'url', 'time_interval_mean',
-                     'time_interval_var']
+                     'time_interval_var',  'followers_num', 'following_num', 'content_length']
     data = raw_file[aval_feat_list]
     target = raw_file[['is_spammer']]
 
