2e9b015d95872cb9af8b42f7383036560d7ce092
Joezc <lzc814310057@163.com>
2017-05-24 20:58:29 +0800

    84.9%

--- djangoUI/mainpage/views.py
+++ djangoUI/mainpage/views.py
@@ -8,11 +8,11 @@ def index(request):
     # t = get_template('index.html')
     # html = t.render(Context(locals()))
     # return HttpResponse(html)
-
-    # return render_to_response('index.html',locals())
-
     ans = ''
-    return render(request, 'index.html', {'ret': ans})
-    #
-    # template = get_template('index.html')
-    # return HttpResponse(template.render(request))
+
+    if request.method == 'POST':
+        uid = request.POST.get('nickname')
+        
+        ans = uid
+
+    return render(request, 'index.html', {"ret": ans})
