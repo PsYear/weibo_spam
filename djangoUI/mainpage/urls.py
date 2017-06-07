723ae188fced79274c07229fe9a9888d22145b26
GenkunAbe <genkunabe@hotmail.com>
2017-06-07 22:00:55 +0800

    HTML with CSS

--- djangoUI/mainpage/urls.py
+++ djangoUI/mainpage/urls.py
@@ -1,7 +1,13 @@
 from django.conf.urls import url
-
+from django.views.static import serve
 from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    
+    url(r'^js/(?P<path>.*)$', serve, { 'document_root': './template/js/' }),
+
+    url(r'^css/(?P<path>.*)$', serve, { 'document_root': './template/css/' }),
+    
+    url(r'^images/(?P<path>.*)$', serve, { 'document_root': './template/images/' }),
 ]
